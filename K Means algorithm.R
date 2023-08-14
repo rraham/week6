3984ffb0fc8529635fc408e807755828d3a2bd8f
rraham <139708381+rraham@users.noreply.github.com>
2023-08-13 22:00:11 -0500

    Add files via upload

---
+++ K Means algorithm.R
@@ -0,0 +1,60 @@
+#K-Means Clustering
+
+require("datasets")
+data("iris") # load Iris Dataset
+str(iris) #view structure of dataset
+
+summary(iris) #view statistical summary of dataset
+
+head(iris, 3) #view top  rows of dataset
+
+#Preprocess the dataset
+#Since clustering is a type of Unsupervised Learning, 
+#we would not require Class Label(output) during execution of our algorithm. 
+#We will, therefore, remove Class Attribute “Species” and store it in another variable. 
+#We would then normalize the attributes between 0 and 1 using our own function.
+
+iris.new<- iris[,c(1,2,3,4)]
+iris.class<- iris[,"Species"]
+head(iris.new, 3)
+
+head(iris.class, 3)
+
+normalize <- function(x){
+  return ((x-min(x))/(max(x)-min(x)))
+}
+
+iris.new$Sepal.Length<- normalize(iris.new$Sepal.Length)
+iris.new$Sepal.Width<- normalize(iris.new$Sepal.Width)
+iris.new$Petal.Length<- normalize(iris.new$Petal.Length)
+iris.new$Petal.Width<- normalize(iris.new$Petal.Width)
+head(iris.new)
+
+#Apply the K-means clustering algorithm
+result<- kmeans(iris.new,3) #aplly k-means algorithm with no. of centroids(k)=3
+result$size # gives no. of records in each cluster
+
+result$centers # gives value of cluster center datapoint value(3 centers for k=3)
+
+result$cluster #gives cluster vector showing the custer where each record falls
+
+#Verify results of clustering
+par(mfrow=c(2,2), mar=c(5,4,2,2))
+plot(iris.new[c(1,2)], col=result$cluster)# Plot to see how Sepal.Length and Sepal.Width data points have been distributed in clusters
+plot(iris.new[c(1,2)], col=iris.class)# Plot to see how Sepal.Length and Sepal.Width data points have been distributed originally as per "class" attribute in dataset
+plot(iris.new[c(3,4)], col=result$cluster)# Plot to see how Petal.Length and Petal.Width data points have been distributed in clusters
+plot(iris.new[c(3,4)], col=iris.class)
+
+table(result$cluster,iris.class)
+
+
+#How did the model do?
+#the K-Means clustering model has achieved 44.6% level of accuracy in classifying 
+#the instances in the Iris dataset.
+#TASK: Accuracy = number of correctly classified/(total classified) = ?
+#i.e our model has achieved ?% accuracy!
+#44.66667 % 
+total_classified <- sum(result$size)
+correctly_classified <- sum(diag(table(result$cluster, iris.class)))
+accuracy <- (correctly_classified / total_classified) * 100
+cat("Our model has achieved", accuracy, "% accuracy!")
